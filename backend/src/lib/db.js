d5474ecb7c412d036f625f59ab94357f8b8a327c
vikas9074122983 <vikas9074122983@gmail.com>
2025-07-25 11:41:13 +0200

    project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
